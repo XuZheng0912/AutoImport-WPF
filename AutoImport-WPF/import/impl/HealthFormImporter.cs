017e0b0a51d3ebc2d87c7d6276c8d9146c10085f
Zheng Xu <slowpolitics@outlook.com>
2024-06-25 20:41:01 +0800

    HealthFormImporter.cs

--- AutoImport-WPF/import/impl/HealthFormImporter.cs
+++ AutoImport-WPF/import/impl/HealthFormImporter.cs
@@ -24,7 +24,7 @@ public class HealthFormImporter : IFileImport, IListDataImport<HealthFormData>
     {
         ReadyForImport();
     }
-    
+
     private static void ReadyForImport()
     {
         var success = false;
@@ -41,7 +41,7 @@ public class HealthFormImporter : IFileImport, IListDataImport<HealthFormData>
             }
         } while (!success);
     }
-    
+
     private static void PrepareForImport()
     {
         OpenWebsite();
@@ -49,7 +49,7 @@ public class HealthFormImporter : IFileImport, IListDataImport<HealthFormData>
         SwitchToPersonalRecordManage();
         SetQueryLimitId();
     }
-    
+
     private static void SwitchToPersonalRecordManage()
     {
         var hrIdBy = By.Id("HR");
@@ -59,7 +59,7 @@ public class HealthFormImporter : IFileImport, IListDataImport<HealthFormData>
         Browser.Wait(moduleIdBy);
         Browser.Click(moduleIdBy);
     }
-    
+
     private static void SetQueryLimitId()
     {
         List<string> limitImgPossibleXpathList =
@@ -70,18 +70,16 @@ public class HealthFormImporter : IFileImport, IListDataImport<HealthFormData>
         Browser.ClickByPossibleXpathList(limitImgPossibleXpathList);
         List<string> idLimitPossibleXpathList =
         [
-            "/html/body/div[12]/div/div[6]",
-            "/html/body/div[17]/div/div[6]",
-            "/html/body/div[8]/div/div[7]"
+            "/html/body/div[9]/div/div[6]"
         ];
         Browser.ClickByPossibleXpathList(idLimitPossibleXpathList);
     }
-    
+
     private static void OpenWebsite()
     {
         Browser.Get(WebConfig.TargetUrl);
     }
-    
+
     private static void Login()
     {
         Browser.SendKeys(By.XPath("//div[@id='usertext']//div//input"), ApplicationContext.Username);
