7d14fdcbbbbc650cc82d68a91516881f5edf0093
Zheng Xu <slowpolitics@outlook.com>
2024-06-25 20:20:38 +0800

    HealthFormImporter.csh

--- AutoImport-WPF/import/impl/HealthFormImporter.cs
+++ AutoImport-WPF/import/impl/HealthFormImporter.cs
@@ -1,8 +1,10 @@
 ﻿using AutoImport_WPF.browser;
 using AutoImport_WPF.config;
+using AutoImport_WPF.context;
 using AutoImport_WPF.domain;
 using AutoImport_WPF.excel;
 using AutoImport_WPF.log;
+using OpenQA.Selenium;
 
 namespace AutoImport_WPF.import.impl;
 
@@ -20,7 +22,76 @@ public class HealthFormImporter : IFileImport, IListDataImport<HealthFormData>
 
     public void Import(List<HealthFormData> dataList)
     {
-        throw new NotImplementedException();
+        ReadyForImport();
+    }
+    
+    private static void ReadyForImport()
+    {
+        var success = false;
+        do
+        {
+            try
+            {
+                PrepareForImport();
+                success = true;
+            }
+            catch (Exception)
+            {
+                Logger.Info("正在登入网站");
+            }
+        } while (!success);
+    }
+    
+    private static void PrepareForImport()
+    {
+        OpenWebsite();
+        Login();
+        SwitchToPersonalRecordManage();
+        SetQueryLimitId();
+    }
+    
+    private static void SwitchToPersonalRecordManage()
+    {
+        var hrIdBy = By.Id("HR");
+        Browser.Wait(hrIdBy);
+        Browser.Click(hrIdBy);
+        var moduleIdBy = By.Id("WL_module_D20");
+        Browser.Wait(moduleIdBy);
+        Browser.Click(moduleIdBy);
+    }
+    
+    private static void SetQueryLimitId()
+    {
+        List<string> limitImgPossibleXpathList =
+        [
+            "/html/body/div[1]/div/div/div[2]/table/tbody/tr[1]/td[3]/div/div[2]/div/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div/div/div/div[1]/div[1]/div/div/div/div[2]/div[1]/div/div/div[1]/div/table/tbody/tr/td[1]/table/tbody/tr/td[1]/div/img",
+            "/html/body/div[1]/div/div/div[2]/table/tbody/tr[1]/td[3]/div/div[2]/div/div/div[2]/div[1]/div/div/div/div[2]/div/div/div/div/div[1]/div[1]/div/div/div/div[2]/div[1]/div/div/div[1]/div/table/tbody/tr/td[1]/table/tbody/tr/td[1]/div/img"
+        ];
+        Browser.ClickByPossibleXpathList(limitImgPossibleXpathList);
+        List<string> idLimitPossibleXpathList =
+        [
+            "/html/body/div[12]/div/div[6]",
+            "/html/body/div[17]/div/div[6]",
+            "/html/body/div[8]/div/div[7]"
+        ];
+        Browser.ClickByPossibleXpathList(idLimitPossibleXpathList);
+    }
+    
+    private static void OpenWebsite()
+    {
+        Browser.Get(WebConfig.TargetUrl);
+    }
+    
+    private static void Login()
+    {
+        Browser.SendKeys(By.XPath("//div[@id='usertext']//div//input"), ApplicationContext.Username);
+        Browser.SendKeys(By.Id("pwd"), ApplicationContext.Password);
+        Browser.Click(By.XPath("//div[@id='select-role']//div//input"));
+        Thread.Sleep(500);
+        Browser.Click(By.XPath("//li[text()='责任医生']//parent::ul"));
+        Thread.Sleep(500);
+        Browser.Click(By.XPath("//li[text()='责任医生']//parent::ul"));
+        Browser.Click(By.Id("logon"));
     }
 
     private static List<HealthFormData> ReadFromExcelFile(string fileName)
