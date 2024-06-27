a1a4eb4ea9b2ef1f8914db70ac709e75b07532df
Zheng Xu <slowpolitics@outlook.com>
2024-06-27 22:08:05 +0800

    Browser.cs

--- AutoImport-WPF/service/impl/Browser.cs
+++ AutoImport-WPF/service/impl/Browser.cs
@@ -10,7 +10,7 @@ public class Browser(IWebDriver webDriver) : IBrowser
 {
     private static ILogger Logger => LogConfig.Logger;
 
-    private readonly WebDriverWait _wait = new(webDriver, TimeSpan.FromSeconds(1));
+    private readonly WebDriverWait _wait = new(webDriver, TimeSpan.FromSeconds(3));
 
     public bool IsOptionSelected(string name)
     {
