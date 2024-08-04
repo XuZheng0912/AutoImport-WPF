d18c1ccf08d38e4ecf67187591db1a297a1345f8
Zheng Xu <slowpolitics@outlook.com>
2024-08-04 23:11:34 +0800

    ExcelRow.cs

--- AutoImport-WPF/domain/ExcelRow.cs
+++ AutoImport-WPF/domain/ExcelRow.cs
@@ -1,11 +1,32 @@
-﻿using NPOI.SS.UserModel;
+﻿using System.Globalization;
+using NPOI.SS.UserModel;
 
 namespace AutoImport_WPF.domain;
 
 public class ExcelRow(IRow row)
 {
-    public string this[int index] => row.GetCell(index).ToString() ?? string.Empty;
-
+    public string this[int index] => GetCellValueAsString(row.GetCell(index)) ?? string.Empty;
+    
+    private static string? GetCellValueAsString(ICell cell)
+    {
+        return cell.CellType switch
+        {
+            CellType.String => cell.StringCellValue,
+            CellType.Numeric => DateUtil.IsCellDateFormatted(cell)
+                ? cell.DateCellValue.ToString()
+                : cell.NumericCellValue.ToString(CultureInfo.InvariantCulture),
+            CellType.Boolean => cell.BooleanCellValue.ToString(),
+            CellType.Formula => cell.CachedFormulaResultType switch
+            {
+                CellType.String => cell.StringCellValue,
+                CellType.Numeric => cell.NumericCellValue.ToString(CultureInfo.InvariantCulture),
+                CellType.Boolean => cell.BooleanCellValue.ToString(),
+                _ => cell.ToString()
+            },
+            _ => cell.ToString()
+        };
+    }
+    
     public double GetNumberValue(int index)
     {
         return row.GetCell(index).NumericCellValue;
