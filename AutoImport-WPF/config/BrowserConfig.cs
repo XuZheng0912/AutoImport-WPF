db61383811614bc3c766537e6bb0520a5ec9151e
Java Heroo <slowpolitics@outlook.com>
2025-01-31 23:31:35 +0800

    firefox驱动

--- AutoImport-WPF/config/BrowserConfig.cs
+++ AutoImport-WPF/config/BrowserConfig.cs
@@ -1,4 +1,5 @@
-﻿using AutoImport_WPF.browser;
+﻿using System.IO;
+using AutoImport_WPF.browser;
 using AutoImport_WPF.service.impl;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
@@ -8,18 +9,17 @@ namespace AutoImport_WPF.config;
 public static class BrowserConfig
 {
     public static string BrowserRegisterKey = @"SOFTWARE\Clients\StartMenuInternet";
-
-    private static readonly string FireFoxDriverPath = "C:\\Users\\Administrator\\Desktop\\geckodriver.exe";
-
+    
     private static IBrowser? _browser;
 
     public static IBrowser Browser
     {
         get
         {
+            var driverPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "geckodriver.exe");
             if (_browser == null)
             {
-                FirefoxDriverService service = FirefoxDriverService.CreateDefaultService(FireFoxDriverPath);
+                FirefoxDriverService service = FirefoxDriverService.CreateDefaultService(driverPath);
                 IWebDriver driver = new FirefoxDriver(service);
                 _browser = new Browser(driver);
                 return _browser;
